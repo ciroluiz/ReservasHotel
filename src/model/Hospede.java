6f20277aac1b41db8823ce37fecd2aa45f0248db
ciroluiz <ciroluiz.cl@gmail.com>
2023-09-01 23:01:29 -0300

    Desenvolvimento das classes Hospede e Reserva.

--- src/model/Hospede.java
+++ src/model/Hospede.java
@@ -1,5 +1,70 @@
 package model;
 
+import java.time.LocalDate;
+
 public class Hospede {
+	private Integer id;
+	private String nome;
+	private String sobrenome;
+	private LocalDate dataNascimento;
+	private String nacionalidade;
+	private String telefone;
+	private Integer idReserva;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getSobrenome() {
+		return sobrenome;
+	}
+
+	public void setSobrenome(String sobrenome) {
+		this.sobrenome = sobrenome;
+	}
+
+	public LocalDate getDataNascimento() {
+		return dataNascimento;
+	}
+
+	public void setDataNascimento(LocalDate dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+
+	public String getNacionalidade() {
+		return nacionalidade;
+	}
+
+	public void setNacionalidade(String nacionalidade) {
+		this.nacionalidade = nacionalidade;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+	public Integer getIdReserva() {
+		return idReserva;
+	}
+
+	public void setIdReserva(Integer idReserva) {
+		this.idReserva = idReserva;
+	}
 
 }
--- src/model/Reserva.java
+++ src/model/Reserva.java
@@ -1,5 +1,52 @@
 package model;
 
+import java.time.LocalDate;
+
 public class Reserva {
+	private Integer id;
+	private LocalDate dataEntrada;
+	private LocalDate dataSaida;
+	private Float valor;
+	private String formaPagamento;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public LocalDate getDataEntrada() {
+		return dataEntrada;
+	}
+
+	public void setDataEntrada(LocalDate dataEntrada) {
+		this.dataEntrada = dataEntrada;
+	}
+
+	public LocalDate getDataSaida() {
+		return dataSaida;
+	}
+
+	public void setDataSaida(LocalDate dataSaida) {
+		this.dataSaida = dataSaida;
+	}
+
+	public Float getValor() {
+		return valor;
+	}
+
+	public void setValor(Float valor) {
+		this.valor = valor;
+	}
+
+	public String getFormaPagamento() {
+		return formaPagamento;
+	}
+
+	public void setFormaPagamento(String formaPagamento) {
+		this.formaPagamento = formaPagamento;
+	}
 
 }
